883c942ccf78561be6c3d2d7665181dd343c3e68
Charlie Erling <charlie.erling@gmail.com>
2018-10-31 13:26:09 -0700

    the commit

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+    {
+      name:"Devin Booker",
+      photo:"https://cdn-s3.si.com/s3fs-public/teams/basketball/nba/players/845564-300x300.png",
+      scores:[
+        "5",
+        "1",
+        "5",
+        "5",
+        "1",
+        "2",
+        "1",
+        "1",
+        "2",
+        "5"
+      ]
+    },
+    {
+      name: "Deandre Ayton",
+      photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/4278129.png&w=350&h=254",
+      scores: [
+        "1",
+        "4",
+        "4",
+        "5",
+        "5",
+        "5",
+        "1",
+        "3",
+        "2",
+        "5"
+      ]
+    },
+    {
+      name: "TJ Warren",
+      photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/2982334.png&w=350&h=254",
+      scores: [
+        "3",
+        "2",
+        "2",
+        "5",
+        "4",
+        "4",
+        "1",
+        "1",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Josh Jackson",
+      photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/4066297.png&w=350&h=254",
+      scores: [
+        "4",
+        "2",
+        "3",
+        "5",
+        "5",
+        "4",
+        "1",
+        "3",
+        "1",
+        "4"
+      ]
+    },
+    {
+      name: "Mikal Bridges",
+      photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/3147657.png&w=350&h=254",
+      scores: [
+        "2",
+        "4",
+        "5",
+        "5",
+        "3",
+        "3",
+        "1",
+        "4",
+        "5",
+        "3"
+      ]
+    },
+    {
+      name: "Elie Okobo",
+      photo: "http://a.espncdn.com/combiner/i?img=/i/headshots/nba/players/full/4230550.png&w=350&h=254",
+      scores: [
+        "4",
+        "3",
+        "3",
+        "5",
+        "2",
+        "1",
+        "1",
+        "5",
+        "2",
+        "5"
+      ]
+    }
+  ]
+
+
+module.exports = friends;
